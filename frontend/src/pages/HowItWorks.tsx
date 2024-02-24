be5b8f90677253602378debf986d36ca6b9cbe61
Rashad-Stack <tex.eng.rashed@gmail.com>
2024-02-24 07:44:57 +0600

    Refactor HowItWorks page layout and content

--- frontend/src/pages/HowItWorks.tsx
+++ frontend/src/pages/HowItWorks.tsx
@@ -1,3 +1,49 @@
 export default function HowItWorks() {
-  return <div>HowItWorks</div>;
+  return (
+    <section>
+      <div className="container mx-auto my-4 max-w-xl">
+        <h1 className="my-6 text-center text-4xl font-bold">How it works</h1>
+        <div className="space-y-2">
+          <p>
+            Every new account have 100 watch points. when some one watched your
+            videos watch points will be decrease.
+          </p>
+          <img src="/1.png" alt="profile" />
+          <p>
+            1 points per one video. when you watch some one video you will get 1
+            point to your account.
+          </p>
+          <p>
+            when your watch points are 0, your videos will not be shown to
+            others. you need to watch others videos to get more watch points.
+          </p>
+          <p>
+            You can also buy watch points. 10 watch points for BDT 1. You can
+            buy watch points from your account. At the moment will only accept
+            bkash send money. to buy watch points.
+          </p>
+          <img src="/3.png" alt="profile" />
+          <p>
+            whose watch points are more, their videos will be shown to the top.
+            You can also see your watch points in your account. You can watch
+            multiple videos at a time.
+          </p>
+          <img src="/4.png" alt="profile" />
+          <p>
+            Our system track the videos for watching videos. when counted viewed
+            you'll see a alert for the added point to you account.
+          </p>
+          <p>
+            You can also add videos to your account. You can also delete videos
+            from your account.
+          </p>
+          <img src="/2.png" alt="profile" />
+          <p>
+            For any query you can contact us. We will try to solve your problem.
+            <a href="tel:+8801758214729">+8801758214729</a>
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
