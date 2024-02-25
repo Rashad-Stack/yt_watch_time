f88b73fbe93285062bdbc73f78fe1bdd29c06095
Rashad-Stack <tex.eng.rashed@gmail.com>
2024-02-25 23:32:23 +0600

    Remove unused code and dependencies

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,7 +3,6 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { GraphQLModule } from "@nestjs/graphql";
 import { MongooseModule } from "@nestjs/mongoose";
-import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
@@ -14,11 +13,6 @@ import { VideoModule } from "./video/video.module";
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, "..", "client"),
-      exclude: ["/graphql/(.*)"],
-    }),
-
     ConfigModule.forRoot(),
 
     MongooseModule.forRoot(process.env.MONGO_URI),
